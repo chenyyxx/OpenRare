5c98a9b6825bbb47dc523237b4cec38390af29f9
chenyyxx <chenyx.eric2013@gmail.com>
2022-03-15 15:17:41 -0700

    finished setting up user authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({
+  Component, pageProps: { session, ...pageProps }
+}: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-    
+    <SessionProvider session={session}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
   )
 }
-
-export default MyApp
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,33 @@
-
 import NextAuth from 'next-auth'
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from 'next-auth/providers/google'
+// import EmailProvider from 'next-auth/providers/email'
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+  providers: [
+    // OAuth authentication providers...
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+  ],
+    // TODO: implement this later
+    // need api from sendgrid
+    // need a company email address
+    // need a 
+    // Passwordless / email sign in
+    // EmailProvider({
+    //   server: process.env.MAIL_SERVER,
+    //   from: 'NextAuth.js <no-reply@example.com>'
+    // }),
+  adapter: PrismaAdapter(prisma),
+  // callbacks: {
+  //   session({ session, token, user }) {
+  //     return session // The return type will match the one returned in `useSession()`
+  //   },
+  // },
+});
