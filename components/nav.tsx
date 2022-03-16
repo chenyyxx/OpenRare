bf189d367d6dbc21d08200f6175380fccc3f7a6b
chenyyxx <chenyx.eric2013@gmail.com>
2022-03-16 14:26:22 -0700

    refactor navbar item into components

---
+++ components/nav.tsx
@@ -0,0 +1,242 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+import React, { ReactNode } from 'react';
+import {
+    IconButton,
+    Avatar,
+    Box,
+    CloseButton,
+    Flex,
+    HStack,
+    VStack,
+    Icon,
+    useColorModeValue,
+    Link,
+    Drawer,
+    DrawerContent,
+    Text,
+    useDisclosure,
+    BoxProps,
+    FlexProps,
+    Menu,
+    MenuButton,
+    MenuDivider,
+    MenuItem,
+    MenuList,
+  } from '@chakra-ui/react';
+  import {
+    FiHome,
+    FiTrendingUp,
+    FiCompass,
+    FiStar,
+    FiSettings,
+    FiMenu,
+    FiBell,
+    FiChevronDown,
+  } from 'react-icons/fi';
+  import { IconType } from 'react-icons';
+  import { ReactText } from 'react';
+
+interface LinkItemProps {
+    name: string;
+    icon: IconType;
+  }
+  const LinkItems: Array<LinkItemProps> = [
+    { name: 'Home', icon: FiHome },
+    { name: 'Trending', icon: FiTrendingUp },
+    { name: 'Explore', icon: FiCompass },
+    { name: 'Favourites', icon: FiStar },
+    { name: 'Settings', icon: FiSettings },
+  ];
+
+export default function Nav(){
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    return (
+        <>
+            <SidebarContent
+                onClose={() => onClose}
+                display={{ base: 'none', md: 'block' }}
+            />
+            <Drawer
+                autoFocus={false}
+                isOpen={isOpen}
+                placement="left"
+                onClose={onClose}
+                returnFocusOnClose={false}
+                onOverlayClick={onClose}
+                size="full">
+                <DrawerContent>
+                <SidebarContent onClose={onClose} />
+                </DrawerContent>
+            </Drawer>
+            {/* mobilenav */}
+            <MobileNav onOpen={onOpen} />
+        </>
+    );
+  }
+
+
+
+  interface SidebarProps extends BoxProps {
+    onClose: () => void;
+  }
+  
+  const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+    return (
+      <Box
+        transition="3s ease"
+        bg={useColorModeValue('white', 'gray.900')}
+        borderRight="1px"
+        borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+        w={{ base: 'full', md: 60 }}
+        pos="fixed"
+        h="full"
+        {...rest}>
+        <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+          <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+            Logo
+          </Text>
+          <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+        </Flex>
+        {LinkItems.map((link) => (
+          <NavItem key={link.name} icon={link.icon}>
+            {link.name}
+          </NavItem>
+        ))}
+      </Box>
+    );
+  };
+  
+  interface NavItemProps extends FlexProps {
+    icon: IconType;
+    children: ReactText;
+  }
+  const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
+    return (
+      <Link href="#" style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
+        <Flex
+          align="center"
+          p="4"
+          mx="4"
+          borderRadius="lg"
+          role="group"
+          cursor="pointer"
+          _hover={{
+            bg: 'cyan.400',
+            color: 'white',
+          }}
+          {...rest}>
+          {icon && (
+            <Icon
+              mr="4"
+              fontSize="16"
+              _groupHover={{
+                color: 'white',
+              }}
+              as={icon}
+            />
+          )}
+          {children}
+        </Flex>
+      </Link>
+    );
+  };
+  
+  interface MobileProps extends FlexProps {
+    onOpen: () => void;
+  }
+  const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+    const { data: session } = useSession();
+    return (
+      <Flex
+        ml={{ base: 0, md: 60 }}
+        px={{ base: 4, md: 4 }}
+        height="20"
+        alignItems="center"
+        bg={useColorModeValue('white', 'gray.900')}
+        borderBottomWidth="1px"
+        borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+        justifyContent={{ base: 'space-between', md: 'flex-end' }}
+        {...rest}>
+        <IconButton
+          display={{ base: 'flex', md: 'none' }}
+          onClick={onOpen}
+          variant="outline"
+          aria-label="open menu"
+          icon={<FiMenu />}
+        />
+  
+        <Text
+          display={{ base: 'flex', md: 'none' }}
+          fontSize="2xl"
+          fontFamily="monospace"
+          fontWeight="bold">
+          Logo
+        </Text>
+  
+        <HStack spacing={{ base: '0', md: '6' }}>
+          <IconButton
+            size="lg"
+            variant="ghost"
+            aria-label="open menu"
+            icon={<FiBell />}
+          />
+          <Flex alignItems={'center'}>
+            <Menu>
+              <MenuButton
+                py={2}
+                transition="all 0.3s"
+                _focus={{ boxShadow: 'none' }}>
+                <HStack>
+                  {
+                    session ?
+                    <Avatar
+                      size={'sm'}
+                      name={session.user.email}
+                      src={
+                        'https://images.user.profile'
+                      } // user upload photo
+                    /> :
+                    <Avatar
+                    size={'sm'}
+                  />
+  
+                  }
+                  
+                  <VStack
+                    display={{ base: 'none', md: 'flex' }}
+                    alignItems="flex-start"
+                    spacing="1px"
+                    ml="2">
+                    {session ? 
+                    <Text fontSize="sm">{session.user.email}</Text> :
+                    <Text fontSize="sm">Not Signed In</Text>
+                    }
+                    {/* <Text fontSize="xs" color="gray.600">
+                      Admin
+                    </Text> */}
+                  </VStack>
+                  <Box display={{ base: 'none', md: 'flex' }}>
+                    <FiChevronDown />
+                  </Box>
+                </HStack>
+              </MenuButton>
+              <MenuList
+                bg={useColorModeValue('white', 'gray.900')}
+                borderColor={useColorModeValue('gray.200', 'gray.700')}>
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>Settings</MenuItem>
+                <MenuItem>Billing</MenuItem>
+                <MenuDivider />
+                {
+                  session ?
+                  <MenuItem onClick={() => signOut()}>Sign Out</MenuItem> :
+                  <MenuItem onClick={() => signIn()}>Sign In / Sign Up</MenuItem>
+                }
+                
+              </MenuList>
+            </Menu>
+          </Flex>
+        </HStack>
+      </Flex>
+    );
+  };
