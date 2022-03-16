659d53e7258a5f75bc18f14bddf98f0acee5839f
chenyyxx <chenyx.eric2013@gmail.com>
2022-03-15 17:43:52 -0700

    fix minor bugs

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,8 +11,8 @@ export default NextAuth({
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!
     }),
   ],
     // TODO: implement this later
